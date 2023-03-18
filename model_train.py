ec70ade4752948595498791e2110fa1bb5c081a5
Vishal Tyagi <visualtaggy@gmail.com>
2023-03-19 03:36:10 +0530

    adding core logic and measuring accuracy

--- model_train.py
+++ model_train.py
@@ -15,3 +15,19 @@ labels = data_set.label
 # Splitting data for training and testing
 x_train, x_test, y_train, y_test = train_test_split(
     data_set["text"], labels, test_size=0.2, random_state=20)
+
+# Initializing TF-ID-Vectorizer where tf = term frequence and IDF = weight of rare words
+# tfIDF = tf * IDF. Basically removing words that occur more than 70% like 'and','this','the' etc
+vector = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+
+tf_train = vector.fit_transform(x_train)
+tf_test = vector.transform(x_test)
+
+passive_aggressive_classifier = PassiveAggressiveClassifier(max_iter=50)
+passive_aggressive_classifier.fit(tf_train, y_train)
+
+y_predict = passive_aggressive_classifier.predict(tf_test)
+score = accuracy_score(y_test, y_predict)
+
+print("The current accuracy of the model is: " + str(round(score*100, 2)) + "%")
