fb3debcbbd6222c5d34a8a8169f190f7caa3e167
Vishal Tyagi <visualtaggy@gmail.com>
2023-03-20 04:05:24 +0530

    adding logic for navbar animation

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,8 +3,9 @@ const burger = document.querySelector(".burger");
 
 //Functions
 function navToggle(e) {
-  gsap.to(".line1", 0.5, { rotate: "45", y: 5 });
-  gsap.to(".line2", 0.5, { rotate: "-45", y: -5 });
+  gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+  gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+  gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
 }
 
 //Event Listener
