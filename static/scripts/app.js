418e2eb3191c112fde70dcf493c74c5734f4a944
Vishal Tyagi <visualtaggy@gmail.com>
2023-03-20 12:37:35 +0530

    reverse animation effect

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,10 +2,19 @@
 const burger = document.querySelector(".burger");
 //Functions
 function navToggle(e) {
-  gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
-  gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
-  gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
-  document.getElementById("logoImg").src = "/static/b-LOGO.png";
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.getElementById("logoImg").src = "/static/b-LOGO.png";
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.getElementById("logoImg").src = "/static/LOGO.png";
+  }
 }
 
 //Event Listener
