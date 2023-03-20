e3493d1e0999f6f85856d22444ee434e5c9a05a1
Vishal Tyagi <visualtaggy@gmail.com>
2023-03-20 12:29:15 +0530

    adding black logo transition

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,11 +1,11 @@
 //Selector
 const burger = document.querySelector(".burger");
-
 //Functions
 function navToggle(e) {
   gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
   gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
   gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+  document.getElementById("logoImg").src = "/static/b-LOGO.png";
 }
 
 //Event Listener
