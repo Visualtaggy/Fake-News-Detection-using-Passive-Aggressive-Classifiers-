7737d13339a3cedeefcdfe3a8864419689ff676a
Vishal Tyagi <visualtaggy@gmail.com>
2023-03-21 17:56:33 +0530

    fixed cursor bug on contact us page

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -9,12 +9,14 @@ function navToggle(e) {
     gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
     document.getElementById("logoImg").src = "/static/b-LOGO.png";
+    document.getElementById("cursor").style.border = "2px solid black";
   } else {
     e.target.classList.remove("active");
     gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
     document.getElementById("logoImg").src = "/static/LOGO.png";
+    document.getElementById("cursor").style.border = "2px solid white";
   }
 }
 
